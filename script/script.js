10f83fb3b31f90571184f303c22c450c0acb072b
Maiara30 <156543831+Maiara30@users.noreply.github.com>
2024-09-24 23:58:41 +0100

    Delete script directory

--- script/script.js
+++
@@ -1,5 +0,0 @@
-// Adicionando um evento de envio de formulário para exemplo
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Formulário enviado com sucesso!');
-});
